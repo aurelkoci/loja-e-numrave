4583f4c3099d38f8c0dcfd83cce173786994d54f
Aurel koci <aurelkoci@gmail.com>
2024-02-18 14:19:59 +0100

    Focus pasi fitohet loja

--- index.js
+++ index.js
@@ -191,9 +191,10 @@ function lojaMbaroi() {
     "disabled:opacity-75 flex w-full  md:w-64 mt-4 justify-center rounded-md bg-gray-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600";
   resetButton = document.createElement("button");
   resetButton.className =
-    "flex w-full  md:w-64 mt-4 justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-bold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600";
+    "reset flex w-full md:w-64 mt-4 justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-bold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600";
   resetButton.textContent = "Fillo një lojë të rre";
   reset.append(resetButton);
+  resetButton.focus();
   resetButton.addEventListener("click", rifilloLojen);
 
   if (
